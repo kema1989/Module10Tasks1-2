87eb7ed126da9d8a21530ce16d4021850a1350aa
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-08-28 21:17:56 +0600

    Добавьте файлы проекта.

---
+++ Module10Tasks1-2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Module10Tasks1_2
+{
+    public interface ISum
+    {
+        int GetSum(int a, int b);
+    }
+    class Calculator : ISum
+    {
+        public int GetSum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    struct CalculateSum : ISum
+    {
+        public int GetSum(int a, int b)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Calculator calculator = new Calculator();
+                Console.WriteLine("Введите первое слагаемое");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе слагаемое");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"{a} + {b} = " + calculator.GetSum(a, b));
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Введено значение неверного формата...");
+                Program.Main(args);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Произошла непредвиденная ошибка...");
+            }
+
+        }
+    }
+}
